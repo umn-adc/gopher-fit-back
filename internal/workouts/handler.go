4ff9de16c84eec08662ac8b135e58fe484966eea
Colton <matro015@umn.edu>
2025-10-30 18:27:00 -0500

    added workout handler

--- internal/workouts/handler.go
+++ internal/workouts/handler.go
@@ -3,7 +3,7 @@ package workouts
 import "net/http"
 
 func Register(mux *http.ServeMux) {
-	
+	mux.HandleFunc("/workouts", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Workouts endpoint"))
+	})
 }
-
-
