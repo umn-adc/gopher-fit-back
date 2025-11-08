b4a2a1c0feced6bde8fa9c6b41f89d0ec6c23c10
Jia Lauber <laube051@umn.edu>
2025-11-08 00:40:51 -0600

    I made an endpoint finally

--- endpoints/practice/mux.go
+++ endpoints/practice/mux.go
@@ -5,6 +5,8 @@ import "net/http"
 func GetServeMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /practice/Bye", dontyellplz)
+
 	// SET UP YOUR ENDPOINTS HERE
 	// mux.HandleFunc("GET /practice/some_endpoint", yourFunction)
 
