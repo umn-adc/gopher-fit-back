1836856248303694a4c3bcdf5d028e8fe1578438
Luke Golobitsh <golob022@umn.edu>
2026-03-03 10:44:14 -0600

    More crud

--- internal/db/db.go
+++ internal/db/db.go
@@ -87,12 +87,12 @@ func InitDB() *sql.DB {
 		user1_id INTEGER NOT NULL,
 		user2_id INTEGER NOT NULL,
 		action_user_id INTEGER NOT NULL,
-		status TEXT CHECK (gender IN ('pending', 'accepted', 'blocked')),
+		status TEXT CHECK (status IN ('pending', 'accepted', 'blocked')),
 		PRIMARY KEY (user1_id, user2_id),
 		CHECK (user1_id < user2_id),
 		FOREIGN KEY (user1_id) REFERENCES users(id) ON DELETE CASCADE,
 		FOREIGN KEY (user2_id) REFERENCES users(id) ON DELETE CASCADE,
-		FOREIGN KEY (action_id) REFERENCES users(id) ON DELETE CASCADE
+		FOREIGN KEY (action_user_id) REFERENCES users(id) ON DELETE CASCADE
 	);
 	`)
 
--- internal/social/friendships.go
+++ internal/social/friendships.go
@@ -5,7 +5,12 @@ import (
 	"net/http"
 )
 
-func (h *Handler) getUserFriends(w http.ResponseWriter, r *http.Request) {
+// @Summary Get users accepted friends
+// @Tags social
+// @Security BearerAuth
+// @Success 200 {array} Friendship
+// @Router /social/friendships [get]
+func (h *Handler) getFriends(w http.ResponseWriter, r *http.Request) {
 	userID, ok := api.GetUserID(w, r)
 	if !ok {
 		return
@@ -14,11 +19,13 @@ func (h *Handler) getUserFriends(w http.ResponseWriter, r *http.Request) {
 	rows, err := h.DB.Query(
 		`SELECT user1_id, user2_id, action_user_id, status
 		FROM friendships
-		WHERE user1_id = ? OR user2_id = ? AND status = 'accepted'`,
+		WHERE
+			user1_id = ? OR user2_id = ?
+			AND status = 'accepted'`,
 		userID)
 
 	if err != nil {
-		api.WriteError(w, http.StatusInternalServerError, "failed to fetch friendships", err)
+		api.WriteError(w, http.StatusInternalServerError, "failed to fetch friends", err)
 		return
 	}
 
@@ -33,7 +40,7 @@ func (h *Handler) getUserFriends(w http.ResponseWriter, r *http.Request) {
 	api.WriteSuccess(w, http.StatusOK, friendships)
 }
 
-func (h *Handler) getUserPendingRequests(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) getOutgoingRequests(w http.ResponseWriter, r *http.Request) {
 	userID, ok := api.GetUserID(w, r)
 	if !ok {
 		return
@@ -42,11 +49,13 @@ func (h *Handler) getUserPendingRequests(w http.ResponseWriter, r *http.Request)
 	rows, err := h.DB.Query(
 		`SELECT user1_id, user2_id, action_user_id, status
 		FROM friendships
-		WHERE action_id = ? AND status = 'pending'`,
+		WHERE
+			action_id = ?
+			AND status = 'pending'`,
 		userID)
 
 	if err != nil {
-		api.WriteError(w, http.StatusInternalServerError, "failed to fetch friendships", err)
+		api.WriteError(w, http.StatusInternalServerError, "failed to fetch outgoing requests", err)
 		return
 	}
 
@@ -59,6 +68,128 @@ func (h *Handler) getUserPendingRequests(w http.ResponseWriter, r *http.Request)
 	rows.Close()
 
 	api.WriteSuccess(w, http.StatusOK, friendships)
+}
+
+func (h *Handler) getIncomingRequests(w http.ResponseWriter, r *http.Request) {
+	userID, ok := api.GetUserID(w, r)
+	if !ok {
+		return
+	}
+
+	rows, err := h.DB.Query(
+		`SELECT user1_id, user2_id, action_user_id, status
+		FROM friendships
+		WHERE
+			NOT action_user_id = ?
+			AND ? IN (user1_id, user2_id)
+			AND status = 'pending'`,
+		userID, userID)
+
+	if err != nil {
+		api.WriteError(w, http.StatusInternalServerError, "failed to fetch incoming requests", err)
+		return
+	}
+
+	friendships := []Friendship{}
+	for rows.Next() {
+		var f Friendship
+		rows.Scan(&f.User1ID, &f.User2ID, &f.ActionUserID, &f.Status)
+		friendships = append(friendships, f)
+	}
+	rows.Close()
+
+	api.WriteSuccess(w, http.StatusOK, friendships)
+}
+
+func (h *Handler) getOutgoingBlocks(w http.ResponseWriter, r *http.Request) {
+	userID, ok := api.GetUserID(w, r)
+	if !ok {
+		return
+	}
+
+	rows, err := h.DB.Query(
+		`SELECT user1_id, user2_id, action_user_id, status
+		FROM friendships
+		WHERE
+			action_id = ?
+			AND status = 'blocked'`,
+		userID)
+
+	if err != nil {
+		api.WriteError(w, http.StatusInternalServerError, "failed to fetch outgoing blocks", err)
+		return
+	}
+
+	friendships := []Friendship{}
+	for rows.Next() {
+		var f Friendship
+		rows.Scan(&f.User1ID, &f.User2ID, &f.ActionUserID, &f.Status)
+		friendships = append(friendships, f)
+	}
+	rows.Close()
+
+	api.WriteSuccess(w, http.StatusOK, friendships)
+}
+
+func (h *Handler) getIncomingBlocks(w http.ResponseWriter, r *http.Request) {
+	userID, ok := api.GetUserID(w, r)
+	if !ok {
+		return
+	}
+
+	rows, err := h.DB.Query(
+		`SELECT user1_id, user2_id, action_user_id, status
+		FROM friendships
+		WHERE 
+			NOT action_user_id = ? 
+			AND ? IN (user1_id, user2_id) 
+			AND status = 'blocked'`,
+		userID, userID)
+
+	if err != nil {
+		api.WriteError(w, http.StatusInternalServerError, "failed to fetch incoming blocks", err)
+		return
+	}
+
+	friendships := []Friendship{}
+	for rows.Next() {
+		var f Friendship
+		rows.Scan(&f.User1ID, &f.User2ID, &f.ActionUserID, &f.Status)
+		friendships = append(friendships, f)
+	}
+	rows.Close()
+
+	api.WriteSuccess(w, http.StatusOK, friendships)
+}
+
+func (h *Handler) getFriendship(w http.ResponseWriter, r *http.Request) {
+	userID, ok := api.GetUserID(w, r)
+	if !ok {
+		return
+	}
+
+	user2ID, ok := api.PathInt(w, r, "user2_id")
+	if !ok {
+		return
+	}
+
+	user1 := min(userID, user2ID)
+	user2 := max(userID, user2ID)
+
+	var friendship Friendship
+	err := h.DB.QueryRow(
+		`SELECT user1_id, user2_id, action_user_id, status
+		FROM friendships
+		WHERE
+			user1_id = ?
+			AND user2_id = ?`,
+		user1, user2).Scan(
+		&friendship.User1ID, &friendship.User2ID, &friendship.ActionUserID, &friendship.Status)
+
+	if err != nil {
+		api.WriteError(w, http.StatusInternalServerError, "Failed to fetch friendship", err)
+		return
+	}
 }
 
 func (h *Handler) addFriendship(w http.ResponseWriter, r *http.Request) {
@@ -75,9 +206,21 @@ func (h *Handler) addFriendship(w http.ResponseWriter, r *http.Request) {
 	user1 := min(friendship.User1ID, friendship.User2ID)
 	user2 := max(friendship.User1ID, friendship.User2ID)
 
+	// Make sure the user is actually making themselves one of the friends
+	if user1 != userID && user2 != userID {
+		api.WriteError(w, http.StatusBadRequest, "invalid userIDs", nil)
+		return
+	}
+
+	// Don't allow a friendship to start as accepted
+	if friendship.Status == "accepted" {
+		api.WriteError(w, http.StatusBadRequest, "Request cannot be instansiated to 'accepted'", nil)
+		return
+	}
+
 	_, err := h.DB.Exec(`
 		INSERT INTO friendships (user1_id, user2_id, action_user_id, status)
-		VALUES (?, ?, ?)
+		VALUES (?, ?, ?, ?)
 	`, user1, user2, userID, friendship.Status)
 
 	if err != nil {
@@ -90,3 +233,54 @@ func (h *Handler) addFriendship(w http.ResponseWriter, r *http.Request) {
 	friendship.User2ID = user2
 	api.WriteSuccess(w, http.StatusCreated, friendship)
 }
+
+// So incredibly insecure
+/*
+Security Cases:
+- User is trying to change their own pending request to accepted
+- User is trying to change anything in another user's block
+*/
+func (h *Handler) updateFriendship(w http.ResponseWriter, r *http.Request) {
+	userID, ok := api.GetUserID(w, r)
+	if !ok {
+		return
+	}
+
+	user2ID, ok := api.PathInt(w, r, "user2_id")
+	if !ok {
+		return
+	}
+
+	var friendship Friendship
+	if !api.DecodeJSON(w, r, &friendship) {
+		return
+	}
+
+	user1 := min(userID, user2ID)
+	user2 := max(userID, user2ID)
+
+	res, err := h.DB.Exec(`
+		UPDATE friendships
+		SET
+			user1_id = ?,
+			user2_id = ?,
+			action_user_id = ?,
+			status = ?
+		WHERE
+			user1_id = ?
+			AND user2_id = ?`,
+		friendship.User1ID, friendship.User2ID, friendship.ActionUserID, friendship.Status, user1, user2)
+
+	if err != nil {
+		api.WriteError(w, http.StatusInternalServerError, "failed to update friendship", err)
+		return
+	}
+
+	if !api.CheckAffected(w, res, "Friendship not found") {
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *Handler) deleteFriendship(w http.ResponseWriter, r *http.Request) {}
